72d41542438d895c76f40d1e85f2fe704b49d4bd
zc <461304314@qq.com>
2019-04-30 17:43:23 +0800

    no message

--- src/script/GameOverItem.ts
+++ src/script/GameOverItem.ts
@@ -5,6 +5,7 @@ export default class GameoverItem extends Laya.Script {
     private rankLab: Laya.Label;
 
     private avatarImg: Laya.Image;
+    private myBg: Laya.Image;
     private nameLab: Laya.Label;
 
     private scoreLab: Laya.Label;
@@ -13,6 +14,7 @@ export default class GameoverItem extends Laya.Script {
     onAwake() { 
         this.rankLab = this.owner.getChildByName("rankLab") as Laya.Label;
         this.avatarImg = this.owner.getChildByName("avatarImg") as Laya.Image;
+        this.myBg = this.owner.getChildByName("myBg") as Laya.Image;
         this.nameLab = this.owner.getChildByName("nameLab") as Laya.Label;
         this.scoreLab = this.owner.getChildByName("scoreLab") as Laya.Label;
     }
@@ -23,7 +25,7 @@ export default class GameoverItem extends Laya.Script {
         if (itemData.avatarUrl && itemData.avatarUrl != "" ) {
             this.avatarImg.skin = itemData.avatarUrl;
         }
-        // this.selfBg.visible = itemData.avatarUrl == UserData.avatarUrl;
+        this.myBg.visible = itemData.avatarUrl == UserData.avatarUrl;
         this.nameLab.text = itemData.nickname ? itemData.nickname : "神秘玩家";
         this.scoreLab.text = itemData.score||0;
         switch (_type) {
